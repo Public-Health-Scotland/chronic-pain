5f111a03ee1d2dfad0d76f9f93ad45c60da21a89
Natalie Polack <natalp01@nssrstudio04.csa.scot.nhs.uk>
2021-09-27 16:09:26 +0100

    Made some of changes suggested by Russell

--- code/01_board_narrative.R
+++ code/01_board_narrative.R
@@ -1,25 +1,32 @@
-##########################################################################################
-###                                                                                    ###
-###   Chronic Pain Publication                                                         ###
-###   Script: 01_combine_board_narrative                                               ###
-###                                                                                    ###
-###   Written for: R Studio Server                                                     ###
-###   R Version: 3.6.1                                                                 ###
-###   Packages required: dplyr(1.0.1), readxl(1.3.1), tidyr(1.1.0), writexl(1.3),      ###
-###                      stringr(1.4.0)                                                ###
-###                                                                                    ###
-###   Description: This script reads the sheet = "NHS Board details",                  ###
-###                range = "C17:C41" from the 14 Board submissions and                 ###
-###                creates a dataframe of one column for the Excel Workbook            ###
-###                                                                                    ###
-##########################################################################################
+################################################################################
+#                                                                                    
+#   Chronic Pain Publication                                                         
+#   Script: 01_combine_board_narrative                                               
+#                                                                                    
+#   Written for: R Studio Server                                                     
+#   R Version: 3.6.1                                                                 
+#   Packages required: dplyr(1.0.1), readxl(1.3.1), tidyr(1.1.0), writexl(1.3),      
+#                      stringr(1.4.0), here(1.0.1)                                                
+#                                                                                    
+#   Description: This script reads the sheet = "NHS Board details",                  
+#                range = "C17:C41" from the 14 Board submissions and                 
+#                creates a dataframe of one column for the Excel Workbook            
+#                                                                                    
+################################################################################
 
-###   1. Get file names   ###
+### 1 - Get file names ----
 
-# Set working directory
-setwd("//PHI_conf/WaitingTimes/Chronic-Pain/Data/Submissions & emails/Current Quarter/")
+# NEED TO GET RID OF THIS setwd( )
+#setwd("//PHI_conf/WaitingTimes/Chronic-Pain/Data/Submissions & emails/Current Quarter/")
+
+# Copy 14 submissions into project folder at start of each publication process?
+
+# HOW DO I tell R to look in the data folder, list the names of the 14 submissions 
+# and ignore the ALL DATA file containing historical data?
 
 # Get list of files
+here:here("data")
+
 filenames <- list.files()
 
 
@@ -34,7 +41,7 @@ for (file in filenames){
       rename(`Board narrative` = `...1`)
 
     # Replace 2 lines of heading text with shorter line of text
-    temp_notes <- temp_notes %>%
+    temp_notes %<>%
       mutate(`Board narrative` = case_when(
         `Board narrative` == "*Please use the space below to update us on your progress to tackle the longest waits/ waiting list queue." ~ "Progress to tackle the longest waits/waiting list queue:", 
         `Board narrative` == "*Please use the space below to add any additional comments to help with interpretation of the data you have submitted this quarter." ~ "Additional comments to aid interpretation of the data submitted this month:",
@@ -50,7 +57,7 @@ for (file in filenames){
     rm(boardname)
     rm(temp_notes)
     
-    notes <- notes %>%
+    notes %<>%
       mutate(`Board narrative` = str_remove_all(`Board narrative`, "\\.xlsx$"))
     
 }
@@ -59,11 +66,18 @@ for (file in filenames){
 ###   3. Write Excel file
 
 # Set path for exporting tables
-# Replace xxxxxxxx with date in format, e.g. 2021-06-30
-path_tables = "//PHI_conf/WaitingTimes/Chronic-Pain/Publications/Outputs/Narrative_xxxxxxxx.xlsx"
+#path_tables = "//PHI_conf/WaitingTimes/Chronic-Pain/Publications/Outputs/Narrative_xxxxxxxx.xlsx"
+#path_tables = "//PHI_conf/WaitingTimes/Chronic-Pain/RAP/chronic-pain-main/data/output/Narrative_xxxxxxxx.xlsx"
+#data <- read_csv(here("./datafolder/datafile.csv"))
+path_tables =  here::here("data", "output", "Narrative_xxxxxxxx.xlsx")
+
+# NEED TO AUTOMATE DATE: Replace xxxxxxxx with date in format, e.g. 2021-06-30
 
 # Write Excel file
 write_xlsx(notes, path_tables)
+
+
+### END OF SCRIPT ###
 
 
 
