c984df1c8200543b8a7903bd058e549f2f2f321d
Natalie Polack <natalp01@nssrstudio06.csa.scot.nhs.uk>
2022-07-25 17:38:07 +0100

    Replaced gather( ) with pivot_longer( )
    Replaced spread( ) with pivot_wider( )
    
    Deleted some commented out code
    
    Added column 1 into the data selection being read for the referrals and removals data and then drop it, to deal with a full row of NAs if this occurs. It would result in an empty dataframe and cbind( ) wouldn't work

--- functions/read_submission.R
+++ functions/read_submission.R
@@ -24,10 +24,6 @@ read_submission <-
       rename(`Report Date` = `current_qtr_end`)
     
     # Read in Board name from the B8 on NHS Board details sheet
-    # board <- read.xlsx(files, sheet = 4, skipEmptyRows = TRUE, 
-    #                    cols = c(2, 2), rows = c(7, 8)) %>%
-    #   rename(`Health Board` = `Health.Board`)
-    
     board <- read.xlsx(files, 
                        sheet = 4, 
                        skipEmptyRows = TRUE, 
@@ -40,14 +36,6 @@ read_submission <-
     ### 2 - Create WaitTime column ----
     
     # Read in WaitTime bands 
-    
-    # waittime <- read.xlsx(files, sheet = 5, skipEmptyRows = FALSE, 
-    #                    cols = 1:108, rows = c(4, 5)) %>%
-    #   select(-`If.there.are.no.patients.then."0".(zero).should.be.entered.`) %>%
-    #   gather(key = "key",
-    #          value = "WaitTime") %>%
-    #   select(-key)
-    
     waittime <- read.xlsx(files, 
                           sheet = 5, 
                           skipEmptyRows = TRUE, 
@@ -58,7 +46,8 @@ read_submission <-
                    values_to = "WaitTime") %>%
       select(-name)
     
-    # Trim trailing white space so that waiting times bands match ALL DATA file
+    # Trim trailing white space, so that text for the waiting time bands 
+    # matches the text in the ALL DATA file
     waittime$WaitTime <- trimws(waittime$WaitTime)
     
     
@@ -66,59 +55,77 @@ read_submission <-
     
     # Read in all adjusted data
     
-    adj <- read.xlsx(files, sheet = 5,
-                     cols = 3:108, rows = c(5:10)) %>%
-      #mutate_at(c(2:106), as.numeric) %>%
-      gather(Wait_Time, count, `0.-.7.days.(<=.1.week)`:`>.728.days.(>105.weeks)`) %>%
+    adj <- read.xlsx(files, 
+                     sheet = 5,
+                     cols = 3:108, 
+                     rows = c(5:10)) %>%
+      pivot_longer(cols = `0.-.7.days.(<=.1.week)`:`>.728.days.(>105.weeks)`,
+                   names_to = "Wait_Time",
+                   values_to = "count") %>%
+      pivot_wider(names_from = "X1",
+                  values_from = "count") %>%
       select(-Wait_Time) %>%
-      mutate(observation = rep(c(1:105), each = 4)) %>% select(observation, everything()) %>%
-      spread(X1, count) %>%
       cbind(waittime) %>%
       rename(`adj patients waiting at pain clinic` = `Pain Clinic - Patients on the waiting list for a 1st appointment/assessment at a pain clinic/service (ongoing waits)`) %>%
       rename(`adj patients waiting at Pain psyc` = `Pain Psychology -  Patients on the waiting list for a 1st appointment/assessment at a pain pyschology service (ongoing waits)`) %>%
       rename(`adj patients seen at pain clinic` = `Pain Clinic - Experienced (actual) waiting time for 1st appointment/assessment at a pain clinic/service (completed waits)`) %>%
       rename(`adj patients seen pain psyc` = `Pain Psychology - Experienced (actual) waiting time for 1st appointment/assessment at a pain clinic/service (completed waits)`) %>%
-      select(WaitTime, `adj patients waiting at pain clinic`, 
-             `adj patients waiting at Pain psyc`, `adj patients seen at pain clinic`,
+      select(WaitTime, 
+             `adj patients waiting at pain clinic`, 
+             `adj patients waiting at Pain psyc`, 
+             `adj patients seen at pain clinic`,
              `adj patients seen pain psyc`)
     
     
     ### 4 - Read in unadjusted data ----
     
     # Read in all unadjusted data
-    unadj <- read.xlsx(files, sheet = 6,
-                       cols = 3:108, rows = c(5:10)) %>%
-      #mutate_at(c(2:106), as.numeric) %>%
-      gather(Wait_Time, count, `0.-.7.days.(<=.1.week)`:`>.728.days.(>105.weeks)`) %>%
+    unadj <- read.xlsx(files, 
+                       sheet = 6,
+                       cols = 3:108, 
+                       rows = c(5:10)) %>%
+      pivot_longer(cols = `0.-.7.days.(<=.1.week)`:`>.728.days.(>105.weeks)`,
+                   names_to = "Wait_Time",
+                   values_to = "count") %>%
+      pivot_wider(names_from = "X1",
+                  values_from = "count") %>%
       select(-Wait_Time) %>%
-      mutate(observation = rep(c(1:105), each = 4)) %>% select(observation, everything()) %>%
-      spread(X1, count) %>%
       cbind(waittime) %>%
       rename(`unadj patients waiting at pain clinic` = `Pain Clinic - Patients on the waiting list for a 1st appointment/assessment at a pain clinic/service (ongoing waits)`) %>%
       rename(`unadj patients waiting at Pain psyc` = `Pain Psychology -  Patients on the waiting list for a 1st appointment/assessment at a pain pyschology service (ongoing waits)`) %>%
       rename(`unadj patients seen at pain clinic` = `Pain Clinic - Experienced (actual) waiting time for 1st appointment/assessment at a pain clinic/service (completed waits)`) %>%
       rename(`unadj patients seen pain psyc` = `Pain Psychology - Experienced (actual) waiting time for 1st appointment/assessment at a pain clinic/service (completed waits)`) %>%
-      select(WaitTime, `unadj patients waiting at pain clinic`, 
-             `unadj patients waiting at Pain psyc`, `unadj patients seen at pain clinic`,
+      select(WaitTime, 
+             `unadj patients waiting at pain clinic`, 
+             `unadj patients waiting at Pain psyc`, 
+             `unadj patients seen at pain clinic`,
              `unadj patients seen pain psyc`)
-    
-    #Use pivot_longer and pivot_wider instead?
-    #  pivot_longer(!X1, names_to = "Wait_Time", values_to = "count", `0.-.7.days.(<=.1.week)`:`>.728.days.(>105.weeks)`)
     
     
     ### 5 - Read in Referrals data ----
     
-    ref_pc <- read.xlsx(files, sheet = 7, skipEmptyRows = TRUE,
-                        cols = 2:5, rows = c(8, 9)) %>%
-      rename(`Pain Clinic Referrals` = `Rejected.referrals`) %>%
-      rename(`Pain clinic rejected referrals` = `Total.referrals`) %>%
+    # Column 1 is included and then dropped incase there is a full row of NAs.
+    # This would create an empty df and cbind() doesn't work
+    
+    # Read in pain clinic data from the Referrals tab
+    ref_pc <- read.xlsx(path, 
+                        sheet = 7, 
+                        skipEmptyRows = TRUE,
+                        cols = 1:5, 
+                        rows = c(8, 9)) %>%
+      select(-X1) %>%
+      rename(`Pain Clinic Referrals` = `Total.referrals`) %>%
+      rename(`Pain clinic rejected referrals` = `Rejected.referrals`) %>%
       rename(`No of 1st Pain Clinic appointments` = `Number.of.patients.seen`) %>%
       rename(`No of 1st appointment pain clinic DNA's` = `Number.of.DNAs.for.1st.appt`)
     
-    # Include column 1 and then drop it to deal with a full row of NAs otherwise
-    # end up with empty df and cbind() doesn't work
-    ref_pp <- read.xlsx(files, sheet = 7, skipEmptyRows = TRUE,
-                        cols = 1:5, rows = c(8, 10)) %>%
+    
+    # Read in pain psychology data from the Referrals tab
+    ref_pp <- read.xlsx(path, 
+                        sheet = 7, 
+                        skipEmptyRows = TRUE,
+                        cols = 1:5, 
+                        rows = c(8, 10)) %>%
       select(-X1) %>%
       rename(`Pain Psychology Referrals` = `Total.referrals`) %>%
       rename(`Pain Psychology rejected referrals` = `Rejected.referrals`) %>%
@@ -128,22 +135,21 @@ read_submission <-
     
     ### 6 - Read in Removals data  ----
     
-    removals <- read.xlsx(files, sheet = 8, skipEmptyRows = TRUE, 
-                          cols = 2:3, rows = c(8, 9)) %>%
+    # Column 1 is included and then dropped incase there is a full row of NAs.
+    # This would create an empty df and cbind() doesn't work
+    
+    # Read in Removals data
+    removals <- read.xlsx(path, 
+                          sheet = 8, 
+                          skipEmptyRows = TRUE, 
+                          cols = 1:3, 
+                          rows = c(8, 9)) %>%
+      select(-X1) %>%
       rename(`Pain clinic removal reasons` = `Removed.from.list.for.Pain.clinic./service`) %>%
       rename(`Pain Psychology removal reasons` = `Removed.from.list.for.Pain.psychology`)
     
     
     ### 7 - Create one dataframe for the Board  ----
-    
-    # current_quarter <- left_join(adj, unadj, by = "WaitTime") %>%
-    #   cbind(ref_pc) %>%
-    #   cbind(ref_pp) %>%
-    #   cbind(removals) %>%
-    #   cbind(date) %>%
-    #   cbind(board) %>%
-    #   select(`Health Board`, `Report Date`, everything()) %>%
-    #   mutate(across(`adj patients waiting at pain clinic`:`Pain Psychology removal reasons`, as.numeric))
     
     current_quarter <- left_join(adj, unadj, by = "WaitTime") %>%
       cbind(ref_pc,
